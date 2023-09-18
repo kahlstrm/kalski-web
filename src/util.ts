99c3857d8a110b52c4e572ce2f8cc3ed4fa085a2
Ahlström Kalle <ahlstrom.kalle@outlook.com>
2023-09-18 12:41:28 +0300

    add prettier

--- src/util.ts
+++ src/util.ts
@@ -4,11 +4,11 @@ import { RepoResponse } from "./types";
 export const fetchReposFromApi = async () => {
   const reposhowcase = repos.map((n) =>
     fetch(`https://api.github.com/repos/${n}`).then((res) =>
-      res.ok ? res.json().then((data) => data as RepoResponse) : null
-    )
+      res.ok ? res.json().then((data) => data as RepoResponse) : null,
+    ),
   );
   const responses = await Promise.all(reposhowcase).then((res) =>
-    res.filter((a) => a != null)
+    res.filter((a) => a != null),
   );
   if (responses.length == 0) {
     throw new Error("couldn't fetch github repos");
